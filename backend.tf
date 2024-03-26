fba4de9eb2fdc7b034192760cd583aa1c7302f29
krishdama <95693543+krishdama@users.noreply.github.com>
2024-03-26 17:45:47 +0530

    Update backend.tf

--- backend.tf
+++ backend.tf
@@ -2,7 +2,7 @@
 terraform {
   backend "azurerm" {
     resource_group_name  = "rg-aks"
-    storage_account_name = "aksdemostorageaccount"
+    storage_account_name = "aksdemostorageaccount1"
     container_name       = "aks"
     key                  = "terraform.tfstate"
   }
